8475a8ef5b63c47210d7da4217c1ab3b7e47d6eb
Nzioki Musau <nzioki@Nziokis-MacBook-Pro.local>
2026-09-17 20:03:56 +0300

    fix(customer): repair OfflineDemoRepository factories after embed

--- test/offline_demo_repository_test.dart
+++ test/offline_demo_repository_test.dart
@@ -3,9 +3,7 @@ import 'package:yole_mobile/services/offline_demo_repository.dart';
 
 void main() {
   test("walletsFor('cust_kasee') length 2", () {
-    final repo = OfflineDemoRepository.createFresh(
-      universePath: 'packages/demo_universe/data/universe.json',
-    );
+    final repo = OfflineDemoRepository.createFresh();
     expect(repo.walletsFor('cust_kasee'), hasLength(2));
   });
 }
